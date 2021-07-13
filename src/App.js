2f386f0313bc8d0ed4fc9c4f02b97a083cb776c8
Sandeep Swami <64160530+sandypswami@users.noreply.github.com>
2021-07-13 20:41:40 +0530

    Changed Title

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 			<Nav />
 			<Banner />
 			<Row
-				title="Netflix Originals"
+				title="NETFLIX Originals"
 				fetchUrl={requests.fetchNetflixOriginals}
 				isLargeRow
 			/>
